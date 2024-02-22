649252f39e3f1890a2bec0ddf30ae157ac63c774
iam-abhinay <160832955+iam-abhinay@users.noreply.github.com>
2024-02-22 21:32:27 +0530

    Update list-users.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -38,7 +38,7 @@ function list_users_with_read_access {
     fi
 }
 function helper{
-expected_cmd_args=2;
+expected_cmd_args=2
 if[ $# -ne expected_cmd_args];then
 ehco "Please execute the script with required cmd args"
 fi
